bc9ee0c0e6e3aec199082c06c3a4488bf4e2d1b9
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2016-05-05 15:42:27 +0530

    Initial commit

---
+++ app/src/main/java/com/sandnunkumara/mapbox/OnlineMaps.java
@@ -0,0 +1,63 @@
+package com.sandnunkumara.mapbox;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+public class OnlineMaps extends AppCompatActivity implements OnMapReadyCallback {
+
+
+
+    private MapView mv;
+    private MapboxMap mapboxMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_maps);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Log.v("mamp", "loading");
+
+        mv = (MapView) findViewById(R.id.mapview);
+        mv.onCreate(savedInstanceState);
+        mv.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(MapboxMap map) {
+        mapboxMap = map;
+        
+
+
+    }
+}
